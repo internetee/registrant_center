1ab2a36df85ec4561387d769db270e1a36f17c66
Martin Peetsalu <martin@we.ee>
2020-07-17 15:57:26 +0300

    fix

--- src/client/components/common/Loading/Loading.js
+++ src/client/components/common/Loading/Loading.js
@@ -1,39 +1,12 @@
 import React from 'react';
 import { Container, Loader } from 'semantic-ui-react';
-import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 
-export function Loading({
-  isLoadingUser,
-  isLoadingDomains,
-  isLoadingContacts
-}) {
-  const Message = () => {
-    if (isLoadingUser) {
-      return <FormattedMessage id="loading.user" tagName="h2" />;
-    }
-    if (isLoadingDomains) {
-      return <FormattedMessage id="loading.domains" tagName="h2" />;
-    }
-    if (isLoadingContacts) {
-      return <FormattedMessage id="loading.contacts" tagName="h2" />;
-    }
-    return <FormattedMessage id="loading.loading" tagName="h2" />;
-  };
-  return (
-    <Container text textAlign="center" className="loading">
-      <Loader size="massive" active />
-      <Message />
-    </Container>
-  );
-}
+const Loading = () => (
+  <Container text textAlign="center" className="loading">
+    <Loader size="massive" active />
+    <FormattedMessage id="loading" tagName="h2" />
+  </Container>
+);
 
-const mapStateToProps = ({ contacts, domains, user }) => {
-  return {
-    isLoadingContacts: contacts && contacts.isLoading,
-    isLoadingDomains: domains && domains.isLoading,
-    isLoading: user && user.isLoading
-  };
-};
-
-export default connect(mapStateToProps)(Loading);
+export default Loading;
